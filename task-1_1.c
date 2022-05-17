328c1a6465349ba7ed1aeca664595b5ffea3c285
kassa97 <57567091+kassa97@users.noreply.github.com>
2022-05-17 14:00:07 +0200

    Update task-1_1.c
    dunno

--- task-1_1.c
+++ task-1_1.c
@@ -1,1 +1,22 @@
 /* Export functions here */
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Function pointers to hold the value of the glibc functions */
+static  ssize_t (*real_write)(int fd, const void *buf, size_t count) = NULL;
+
+/* wrapping write function call */
+ssize_t write(int fd, const void *buf, size_t count)
+{
+
+    /* printing out the number of characters */
+    printf("write:chars#:%lu\n", count);
+    /* reslove the real write function from glibc
+     * and pass the arguments.
+     */
+    real_write = dlsym(RTLD_NEXT, "write");
+    real_write(fd, buf, count);
+
+}
